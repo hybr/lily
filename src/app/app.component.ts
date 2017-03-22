4326001b5f593692a868386a6422d19005711093
Yogesh Sharma <sharma.yogesh.1234@gmail.com>
2017-03-22 16:48:42 +0530

    Added material theme

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {MdToolbarModule} from '@angular/material/toolbar';
 
 @Component({
   selector: 'app-root',
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { MaterialModule } from '@angular/material';
+import 'hammerjs';
+
+import {MdCardModule} from '@angular/material/card';
+import {MdButtonModule} from '@angular/material/button';
+import {MdIconModule} from '@angular/material/icon';
+import {MdIconRegistry} from '@angular/material/icon'
+import {MdToolbarModule} from '@angular/material/toolbar';
 
 import { AppComponent } from './app.component';
 
@@ -12,9 +20,11 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    MaterialModule,
+    MdToolbarModule
   ],
-  providers: [],
+  providers: [ MdIconRegistry ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
