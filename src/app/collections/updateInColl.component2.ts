61d14f9014d67c6b5f57b832e1ecbeee8820c2fc
Yogesh Sharma <yogesh.sharma@gridpoint.com>
2017-04-21 22:15:11 +0530

    Working on ac crud

--- src/app/collections/updateInColl.component2.ts
+++ src/app/collections/updateInColl.component2.ts
@@ -1,5 +1,6 @@
 
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormArray, FormControl, FormGroup, Validators  } from '@angular/forms';
 import { AngularFire, FirebaseObjectObservable, FirebaseListObservable, } from 'angularfire2';
 import { ActivatedRoute, Params } from '@angular/router';
 import { LocalStorageService } from 'angular-2-local-storage';
@@ -14,27 +15,28 @@ import { Router } from '@angular/router';
 })
 export class UpdateDocInCollComponent implements OnInit {
   public fields: Array<any> = <any>[];
-
+  public docForm: FormGroup;
   public listOfCollToUpdate: FirebaseListObservable<any[]>;
   public docOfCocs: FirebaseObjectObservable<CollectionOfCollections>;
-
   public submitted: boolean = false;
 
   constructor(
+    private _fb: FormBuilder,
     private _af: AngularFire,
     private _route: ActivatedRoute,
     private _lss: LocalStorageService
   ) {}
 
   removeSubFieldL1(i, fieldName) {
-    (this.fields[fieldName]).removeAt(i);
+    (<FormArray>this.docForm.controls[fieldName]).removeAt(i);
   }
 
-  addSubFieldL1(fieldName, subFields) {
-    this.fields[fieldName]['value'] = subFields;
+  addSubFieldL1(fieldName, fieldGroup) {
+    const control = <FormArray>this.docForm.controls[fieldName];
+    control.push(this._fb.group(this.docForm['controls'][fieldName]['controls'][0]['controls']));
   }
   
-  createFieldsFromTable(cocsRecord) {
+  getFormControlGroup(cocsRecord) {
     let self = this;
     console.log('cocsRecord received = ', cocsRecord);
     let formFieldsGroup = {};
@@ -66,15 +68,17 @@ export class UpdateDocInCollComponent implements OnInit {
 
       if (fType == 'fields') {
 
-        let groupObject = self.createFieldsFromTable(
+        let groupObject = self.getFormControlGroup(
           cocsRecord[property]['fields']
         );
 
+        formFieldsGroup[property] = self._fb.array([
+           groupObject.fbg
+        ]);
         localFields.push({
           'name' : property,
           'title' : fTitle,
-          'subFields': groupObject.lf,
-          'value': fValue,
+          'value': groupObject.lf,
           'type': fType,
           'sequence': fSequence
         });
@@ -86,6 +90,7 @@ export class UpdateDocInCollComponent implements OnInit {
           'type': fType,
           'sequence': fSequence
         });
+        formFieldsGroup[property] = new FormControl(fValue, []);
       }
 
     } /* for */
@@ -94,30 +99,65 @@ export class UpdateDocInCollComponent implements OnInit {
         return a.sequence - b.sequence;
     });
 
-    return {lf: localFields};
+    return {fbg: self._fb.group(formFieldsGroup), lf: localFields};
   } /* getFormControlGroup */
 
-  private applyFormValues (fields, formValues) {
+  private applyFormValues (group, formValues, isFromArray) {
     console.log('-------------------------------------');
-    console.log('applyFormValues fields = ', fields);
+    console.log('applyFormValues isFromArray = ', isFromArray);
+    console.log('applyFormValues group = ', group);
     console.log('applyFormValues formValues = ', formValues);
 
-    for (var i in fields) {
-      console.log('=====================================');
-      console.log('Updating values for ', i, fields[i]);
-      if (fields[i].hasOwnProperty('subFields')) {
-          fields[i]['value'] = [];
-          for (var j in formValues[fields[i]['name']]) {
-            fields[i]['value'][j]  = this.applyFormValues (fields[i]['subFields'], formValues[fields[i]['name']][j]);
-          }
-
-      } else {
-        fields[i]['value'] = formValues[fields[i]['name']];
-        console.log('Assigned value = ', formValues[fields[i]['name']]);
-      }
+    let controls = group.controls;
+    if (isFromArray>0) {
+        controls = group['controls'][0]['controls'];
     }
 
-    return fields;
+    for (var mainGroupKey in controls) {
+      console.log('=================', mainGroupKey + " -> ", controls[mainGroupKey]);
+
+      if (controls[mainGroupKey] instanceof FormGroup) { /* checking controls[mainGroupKey] */
+
+        for (var subGroupKey in controls[mainGroupKey]['controls']) {
+          console.log('subGroups in FormGroup = ', formValues[mainGroupKey][subGroupKey]);
+          controls[mainGroupKey]['controls'] = this.applyFormValues(
+            controls[mainGroupKey]['controls'], 
+            formValues[mainGroupKey][subGroupKey], 
+            0
+          );
+        }
+
+      } else if (controls[mainGroupKey] instanceof FormArray) { /* checking controls[mainGroupKey] */
+      
+        let count = 1;
+        let controlsArray = [];
+        for (var i in formValues[mainGroupKey]) {
+          console.log('Element in FormArray = ', i, formValues[mainGroupKey][i], ' count = ', count);
+          // self.docForm = self.applyFormValues(self.docForm, docToUpdateRecord, 0);
+          controlsArray[count] = this.applyFormValues(
+            controls[mainGroupKey], 
+            formValues[mainGroupKey][i], 
+            count
+          );
+           
+          count++;
+        }
+        for (var j in controlsArray) {
+           controls[mainGroupKey]['controls'][j] = controlsArray[j];
+        }
+
+
+      } else { /* checking controls[mainGroupKey] */
+        
+        console.log('Updating values for FormControl ' + mainGroupKey + ' = ', controls[mainGroupKey]);
+        controls[mainGroupKey].setValue(formValues[mainGroupKey]);
+      
+      } /* checking controls[mainGroupKey] */
+
+    } /* for (var mainGroupKey in controls) */
+
+
+    return controls;
   }
 
   ngOnInit() {
@@ -136,23 +176,24 @@ export class UpdateDocInCollComponent implements OnInit {
     self.docOfCocs = self._af.database.object(`/c3/${cDocKey}`);
     console.log('self.docOfCocs = ', self.docOfCocs);
     
+
     let docToUpdate = self._af.database.object(`/${cNum}/${docId}`);
+
     console.log('docToUpdate = ', docToUpdate);
 
     self.docOfCocs.subscribe(
       function(cocsRecord) {
         // create class from json and assign to form
-        let groupObject  = self.createFieldsFromTable(cocsRecord['fields']);
-        self.fields = groupObject.lf; // TODO check if this is issue with formbuilder
-
-        console.log('after createFieldsFromTable self.fields = ', self.fields);
-
+        let groupObject  = self.getFormControlGroup(cocsRecord['fields']);
+        self.docForm = groupObject.fbg;
+        console.log('docForm = ', self.docForm);
+        self.fields.push(groupObject.lf);
         // get the values and assign to form
         docToUpdate.subscribe(
           function (docToUpdateRecord) {
             console.log('docToUpdateRecord = ', docToUpdateRecord);
-            self.fields = self.applyFormValues(self.fields, docToUpdateRecord);
-            console.log('docForm after applying values from db = ', self.fields);
+            self.docForm = self.applyFormValues(self.docForm, docToUpdateRecord, 0);
+            console.log('docForm after applying values from db = ', self.docForm);
           } /* function (docToUpdateRecord) */
         ); /* docToUpdate.subscribe */
 
@@ -161,8 +202,8 @@ export class UpdateDocInCollComponent implements OnInit {
     
   } // ngOnInit
 
-  onSubmit(model) {
-    // this.listOfCollToUpdate.push(model.value);
+  onSubmit(model: FormGroup) {
+    this.listOfCollToUpdate.push(model.value);
     console.log('model = ', model)
   } // onSubmit
 }
